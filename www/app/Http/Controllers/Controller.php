f056c52822a492d814fb031af30c1a2997c29fc2
develpr <kevin@develpr.com>
2015-04-28 21:51:34 -0400

    Debugging

--- www/app/Http/Controllers/Controller.php
+++ www/app/Http/Controllers/Controller.php
@@ -1,9 +1,12 @@
 <?php namespace App\Http\Controllers;
 
+use Develpr\AlexaApp\Request\IntentRequest;
 use Develpr\AlexaApp\Response\AlexaResponse;
 use Develpr\AlexaApp\Response\Card;
 use Develpr\AlexaApp\Response\Speech;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+
 
 class Controller extends BaseController
 {
@@ -13,8 +16,9 @@ class Controller extends BaseController
 		"ramen",
 		"garbage"
 	];
-    public function launchRequest()
+    public function launchRequest(Request $request)
 	{
+		\Log::info($request->getContent());
 		$alexaResponse = new AlexaResponse();
 		$speech = new Speech('Welcome to the meal planning app');
 		$card = new Card("Thank You - Meal App", "", "Thanks for using the meal app!");
@@ -24,13 +28,21 @@ class Controller extends BaseController
 		return $alexaResponse;
 	}
 
-	public function listMeals()
+	public function listMeals(Request $request)
 	{
-		$words = "You can chose from ";
+		\Log::info($request->getContent());
+		$alexaResponse = new AlexaResponse();
+		
+		$words = "You can choose from ";
 
-		$words .= implode(", ", array_keys($this->meals));
+		$words .= implode(", ", $this->meals);
 
 		return new AlexaResponse(new Speech($words));
-		
+
+	}
+
+	public function chooseMeal(IntentRequest $intentRequest)
+	{
+
 	}
 }
