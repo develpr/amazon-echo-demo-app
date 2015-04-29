3af70fe95d49ef85f34716bbd9c48ba3036335ba
develpr <kevin@develpr.com>
2015-04-28 20:56:32 -0400

    demo commit

--- www/app/Http/Controllers/Controller.php
+++ www/app/Http/Controllers/Controller.php
@@ -1,8 +1,19 @@
 <?php namespace App\Http\Controllers;
 
+use Develpr\AlexaApp\Response\AlexaResponse;
+use Develpr\AlexaApp\Response\Speech;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function launchRequest()
+	{
+		$alexaResponse = new AlexaResponse();
+		$speech = new Speech('Welcome to the meal planning app');
+
+		$alexaResponse->setSpeech($speech);
+
+		return $alexaResponse;
+
+	}
 }
