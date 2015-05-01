60abbd034656926d9f9e0c5ceea75cf6dca79f3a
develpr <kevin@develpr.com>
2015-05-01 13:23:24 -0400

    added launch logic

--- www/app/Http/Controllers/Controller.php
+++ www/app/Http/Controllers/Controller.php
@@ -1,8 +1,30 @@
 <?php namespace App\Http\Controllers;
 
+use Develpr\AlexaApp\Response\AlexaResponse;
+use Develpr\AlexaApp\Response\Card;
+use Develpr\AlexaApp\Response\Speech;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+	private $meals = [
+		"pizza" => "Pizza is very healthy, great choice!",
+		"sashimi" => "What a fancy person you are! You must be rich!!",
+		"ramen" => "Awesome choice, I'd recommend a tonkotsu ramen!"
+	];
+
+    public function launch()
+	{
+		$alexaResponse = new AlexaResponse;
+
+		$wordsToSpeak = "Hello! Your choices for a meal include " . implode(', ', array_keys($this->meals));
+
+		$speech = new Speech($wordsToSpeak);
+
+		$card = new Card("Hello!", "Your choices", $wordsToSpeak);
+
+		$alexaResponse->setSpeech($speech)->setCard($card);
+
+		return $alexaResponse;
+	}
 }
--- www/app/Http/routes.php
+++ www/app/Http/routes.php
@@ -14,3 +14,6 @@
 $app->get('/', function() use ($app) {
     return $app->welcome();
 });
+
+
+$app->launch('/demo', 'App\Http\Controllers\Controller@launch');
